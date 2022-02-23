829c981dd198a1fd3a933ec56ae8f6f3344758fc
suneel <60563412+suneel00@users.noreply.github.com>
2022-02-23 11:03:15 +0530

    initial commit

---
+++ src/main/java/com/ms/codepipeline/CdeAwsCodepipelineGitApplication.java
@@ -0,0 +1,13 @@
+package com.ms.codepipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdeAwsCodepipelineGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdeAwsCodepipelineGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ms/codepipeline/CdeAwsCodepipelineGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.codepipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdeAwsCodepipelineGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
