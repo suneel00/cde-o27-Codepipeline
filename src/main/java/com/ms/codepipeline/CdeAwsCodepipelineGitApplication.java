46648de4dacf463e4c2dedf215cafab20cff2c19
suneel <60563412+suneel00@users.noreply.github.com>
2022-02-23 12:51:25 +0530

    build spec is added

--- src/main/java/com/ms/codepipeline/CdeAwsCodepipelineGitApplication.java
+++ src/main/java/com/ms/codepipeline/CdeAwsCodepipelineGitApplication.java
@@ -1,10 +1,23 @@
 package com.ms.codepipeline;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CdeAwsCodepipelineGitApplication {
+	@GetMapping("/")
+	public String hello()
+	{
+		return "HelloWorld";
+	}
+	@GetMapping("/welcome")
+	public String welcome()
+	{
+		return "Code pipeline";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CdeAwsCodepipelineGitApplication.class, args);
